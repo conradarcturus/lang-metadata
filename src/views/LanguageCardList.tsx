39fcf3c02bde4b0ac411f4ed22dfbd07da372405
Conrad Nied <conrad.logos@gmail.com>
2025-04-21 15:07:17 -0700

    Enable filtering on Territories as well

--- src/views/LanguageCardList.tsx
+++ src/views/LanguageCardList.tsx
@@ -7,8 +7,9 @@ import { usePageParams } from '../controls/PageParamsContext';
 
 const LanguageCardList: React.FC = () => {
   const { languagesByCode } = useDataContext();
-  const { code, nameFilter } = usePageParams();
+  const { code: codeFilter, nameFilter } = usePageParams();
   const lowercaseNameFilter = nameFilter.toLowerCase();
+  const lowercaseCodeFilter = codeFilter.toLowerCase();
 
   return (
     <div className="CardList">
@@ -16,7 +17,7 @@ const LanguageCardList: React.FC = () => {
         .map((languageCode) => languagesByCode[languageCode])
         .filter(
           (lang) =>
-            (code == '' || lang.code.includes(code)) &&
+            (codeFilter == '' || lang.code.toLowerCase().includes(lowercaseCodeFilter)) &&
             (nameFilter == '' || lang.nameDisplay.toLowerCase().includes(lowercaseNameFilter)),
         )
         .sort((a, b) => b.populationCited - a.populationCited)
--- src/views/TerritoryCardList.tsx
+++ src/views/TerritoryCardList.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import ViewCard from './Cards/ViewCard';
 import TerritoryCard from './Cards/TerritoryCard';
 import { useDataContext } from '../dataloading/DataContext';
+import { usePageParams } from '../controls/PageParamsContext';
 
 const TerritoryCardList: React.FC = () => {
   const { territoriesByCode } = useDataContext();
+  const { code: codeFilter, nameFilter } = usePageParams();
+  const lowercaseNameFilter = nameFilter.toLowerCase();
+  const lowercaseCodeFilter = codeFilter.toLowerCase();
 
   return (
     <div className="CardList">
       {Object.keys(territoriesByCode)
         .map((territoryCode) => territoriesByCode[territoryCode])
+        .filter(
+          (territory) =>
+            (codeFilter === '' || territory.code.toLowerCase().startsWith(lowercaseCodeFilter)) &&
+            (nameFilter === '' ||
+              territory.nameDisplay.toLowerCase().includes(lowercaseNameFilter)),
+        )
         .sort((a, b) => b.population - a.population)
         .map((territory) => (
           <ViewCard key={territory.code}>
