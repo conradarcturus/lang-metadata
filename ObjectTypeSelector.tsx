196e260b56b4ef06841a04da078557812be93894
Yashwanth Kumar Lokrey <59729990+YashwanthKumar49@users.noreply.github.com>
2025-06-01 21:31:09 -0600

    Add files via upload

---
+++ ObjectTypeSelector.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback } from 'react';
+
+import { toTitleCase } from '../../generic/stringUtils';
+import { useMediaQuery } from '../../generic/useMediaQuery';
+import { ObjectType, View } from '../../types/PageParamTypes';
+import { getObjectTypeLabelPlural } from '../../views/common/getObjectName';
+import Selector from '../components/Selector';
+import SingleChoiceOptions from '../components/SingleChoiceOptions';
+import { usePageParams } from '../PageParamsContext';
+
+const objectAmbiguousViews = [View.About, View.Details];
+
+const ObjectTypeSelector: React.FC = () => {
+  const { objectType, replaceAllParams, view } = usePageParams();
+  const isCompact = useMediaQuery('(max-width: 1015px)');
+
+  const goToObjectType = useCallback(
+    (newObjectType: ObjectType) => {
+      const targetView = objectAmbiguousViews.includes(view) ? View.CardList : view;
+
+      console.log(`ObjectTypeSelector: Changing objectType to ${newObjectType}. Resetting all params.`);
+
+      replaceAllParams({
+        objectType: newObjectType,
+        view: targetView,
+        page: 1,
+        searchString: '',
+      });
+    },
+    [replaceAllParams, view],
+  );
+
+  return (
+    <Selector selectorLabel={isCompact ? 'Data:' : undefined} appearance="tabs">
+      <SingleChoiceOptions<ObjectType>
+        options={Object.values(ObjectType)}
+        onChange={goToObjectType}
+        mode={isCompact ? 'dropdown' : 'flat'}
+        selected={objectType}
+        getOptionLabel={(d) => toTitleCase(getObjectTypeLabelPlural(d))}
+        getOptionDescription={(objectType) => (
+          <>
+            <div style={{ marginBottom: 8 }}>Click here to change the kind of entity viewed.</div>
+            <OptionDescription objectType={objectType} />
+          </>
+        )}
+      />
+    </Selector>
+  );
+};
+
+const OptionDescription: React.FC<{ objectType: ObjectType }> = ({ objectType }) => {
+  switch (objectType) {
+    case ObjectType.Language:
+      return (
+        <>
+          <label>Language (Languoid):</label>A verbal communication system used by multiple people.
+          Languages should be are mutually intelligible, whereas a dialect is a subset of a language
+          defined by differences in lexicon and pronounciation. Since languages families, contested
+          languages, and dialects are included it is more precise to consider these
+          &quot;Languoids&quot;.
+        </>
+      );
+    case ObjectType.Locale:
+      return (
+        <>
+          <label>Locale:</label>The combination of a language and territory -- used to express how
+          many people speak a language in a given area or if a language is officially supported.
+          Some locales specific a particular writing system and/or variation information (dialect,
+          orthography...).
+        </>
+      );
+    case ObjectType.Territory:
+      return (
+        <>
+          <label>Territory:</label>A geographical unit. Some may not have universal recognition.
+          Currently showing both countries as well as dependencies (eg. Hong Kong) that have
+          separate ISO codes.
+        </>
+      );
+    case ObjectType.WritingSystem:
+      return (
+        <>
+          <label>Writing System:</label>A system for writing a language to a persistent visual
+          media. For instance Latin alphabet, Han characters, cursive Arabic script. Some systems
+          may contain other systems.
+        </>
+      );
+  }
+};
+
+export default ObjectTypeSelector;
---
+++ PageParamsContext.tsx
@@ -0,0 +1,137 @@
+import React, { createContext, useContext, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { LanguageSchema } from '../types/LanguageTypes';
+import {
+  ObjectType,
+  PageParamKey,
+  PageParams,
+  PageParamsOptional,
+  SearchableField,
+  SortBy,
+  View,
+} from '../types/PageParamTypes';
+import { ScopeLevel } from '../types/ScopeLevel';
+
+type PageParamsContextState = PageParams & {
+  updatePageParams: (newParams: PageParamsOptional) => void;
+  replaceAllParams: (newParams: PageParamsOptional) => void;
+};
+
+const PageParamsContext = createContext<PageParamsContextState | undefined>(undefined);
+const DEFAULT_OBJECT_TYPE = ObjectType.Language;
+const DEFAULT_VIEW = View.CardList;
+const PARAMS_THAT_CLEAR: PageParamKey[] = ['limit', 'page', 'searchString', 'searchBy'];
+
+export const PageParamsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [pageParams, setPageParams] = useSearchParams({});
+
+  const getParam = (key: string, fallback: string = '') => pageParams.get(key) ?? fallback;
+
+  const updatePageParams = (newParams: PageParamsOptional) => {
+    setPageParams((prev) => getNewURLSearchParams(newParams, prev));
+  };
+
+  const replaceAllParams = (newParams: PageParamsOptional) => {
+    setPageParams(getNewURLSearchParams(newParams));
+  };
+
+  const providerValue: PageParamsContextState = useMemo(() => {
+    const objectType = getParam('objectType', DEFAULT_OBJECT_TYPE) as ObjectType;
+    const view = getParam('view', DEFAULT_VIEW) as View;
+    const defaults = getDefaultParams(objectType, view);
+
+    return {
+      languageSchema: getParam('languageSchema', defaults.languageSchema) as LanguageSchema,
+      limit: parseInt(getParam('limit') || defaults.limit.toString(), 10),
+      localeSeparator: getParam('localeSeparator', '') === '-' ? '-' : '_',
+      objectID: getParam('objectID', undefined),
+      objectType,
+      page: parseInt(getParam('page') || defaults.page.toString(), 10),
+      scopes: getParam('scopes', defaults.scopes.join(','))
+        .split(',')
+        .map((s) => s as ScopeLevel)
+        .filter(Boolean),
+      searchBy: getParam('searchBy', defaults.searchBy) as SearchableField,
+      searchString: getParam('searchString', defaults.searchString),
+      sortBy: getParam('sortBy', defaults.sortBy) as SortBy,
+      view,
+      updatePageParams,
+      replaceAllParams,
+    };
+  }, [
+    pageParams,
+    updatePageParams,
+    replaceAllParams,
+  ]);
+
+  return <PageParamsContext.Provider value={providerValue}>{children}</PageParamsContext.Provider>;
+};
+
+function getDefaultParams(objectType: ObjectType, view: View): PageParams {
+  return {
+    languageSchema: LanguageSchema.Inclusive,
+    limit: view === View.Table ? 200 : 8,
+    localeSeparator: '_',
+    objectID: undefined,
+    objectType,
+    page: 1,
+    scopes:
+      view === View.Hierarchy && objectType !== ObjectType.Locale
+        ? [ScopeLevel.Groups, ScopeLevel.Individuals]
+        : [ScopeLevel.Individuals],
+    searchBy: SearchableField.AllNames,
+    searchString: '',
+    sortBy: SortBy.Population,
+    view,
+  };
+}
+
+export function getNewURL(params: PageParamsOptional): string {
+  return `?${getNewURLSearchParams(params).toString()}`;
+}
+
+function getNewURLSearchParams(
+  newParams: PageParamsOptional,
+  prev?: URLSearchParams,
+): URLSearchParams {
+  const next = new URLSearchParams(prev);
+  Object.entries(newParams).forEach(([key, value]) => {
+    if (['limit', 'page'].includes(key)) {
+      const valueAsNumber = parseInt(value as string);
+      if (isNaN(valueAsNumber) || valueAsNumber < 1) {
+        next.set(key, '0');
+      } else {
+        next.set(key, valueAsNumber.toString());
+      }
+    } else if (Array.isArray(value)) {
+      if (value.length === 0) {
+        next.delete(key);
+      } else {
+        next.set(key, value.join(','));
+      }
+    } else if (value == null || value == '') {
+      next.delete(key);
+    } else {
+      next.set(key, value.toString());
+    }
+  });
+  const defaults = getDefaultParams(
+    (next.get('objectType') as ObjectType) ?? DEFAULT_OBJECT_TYPE,
+    (next.get('view') as View) ?? DEFAULT_VIEW,
+  );
+  PARAMS_THAT_CLEAR.forEach((param: PageParamKey) => {
+    if (next.get(param) == defaults[param]?.toString()) {
+      next.delete(param);
+    }
+  });
+  return next;
+}
+
+export const usePageParams = () => {
+  const context = useContext(PageParamsContext);
+  if (!context) {
+    throw new Error('usePageParams must be used within a PageParamsProvider');
+  }
+  return context;
+};
