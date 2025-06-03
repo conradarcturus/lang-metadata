c6a0dc1e248e5418871fce6c0fa902bd77dec619
Yuu Harada <61570874+wizardsimms@users.noreply.github.com>
2025-06-03 13:41:05 -0600

    commits (#41)
    Co-authored-by: Conrad Nied <conrad@unicode.org>
    Co-authored-by: Sri Vijay Bhargav Noone <42865573+noonebhargav@users.noreply.github.com>

--- src/controls/PageNavBar.tsx
+++ src/controls/PageNavBar.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 
-import { View } from '../types/PageParamTypes';
-
 import ControlsBar from './ControlsBar';
-import { usePageParams } from './PageParamsContext';
 import ObjectTypeSelector from './selectors/ObjectTypeSelector';
 import SearchBar from './selectors/SearchBar';
 import ViewSelector from './selectors/ViewSelector';
@@ -11,7 +8,6 @@ import ViewSelector from './selectors/ViewSelector';
 import './controls.css';
 
 const PageNavBar: React.FC = () => {
-  const { view } = usePageParams();
   const [showFilters, setShowFilters] = useState(false);
 
   return (
@@ -25,20 +21,18 @@ const PageNavBar: React.FC = () => {
         <ObjectTypeSelector />
         <ViewSelector />
       </div>
-      {view !== View.About && (
-        <span className="Options">
-          <SearchBar />
-          {showFilters && <ControlsBar />}
-          <div className="selector rounded">
-            <button
-              className={showFilters ? 'selected' : ''}
-              onClick={() => setShowFilters((prev) => !prev)}
-            >
-              Options {showFilters ? `▲` : `▶`}
-            </button>
-          </div>
-        </span>
-      )}
+      <span className="Options">
+        <SearchBar />
+        {showFilters && <ControlsBar />}
+        <div className="selector rounded">
+          <button
+            className={showFilters ? 'selected' : ''}
+            onClick={() => setShowFilters((prev) => !prev)}
+          >
+            Options {showFilters ? `▲` : `▶`}
+          </button>
+        </div>
+      </span>
     </div>
   );
 };
