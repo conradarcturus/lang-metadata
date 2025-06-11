b4192cf33979797f1fdee065eddcfda2bc0fbc1d
Conrad Nied <conrad@translationcommons.org>
2025-06-11 08:35:12 -0700

    Population: Add estimate for how many people write each language (#53)
    Most population estimates are for the number of people that speak a
    language -- but some people that want this data want to know how many
    people use their phones or keyboards in the language. While we do not
    yet have a direct source for written literacy, this approximates it
    using the national literacy rate.
    
    Example from this URL
    
    https://translation-commons.github.io/lang-nav/?view=Details&objectType=Locale&objectID=mai_NP
    
    |Before|After|
    |--|--|
    |![Screenshot 2025-06-06 at 14 02
    21](https://github.com/user-attachments/assets/273f7ede-2f1b-436e-a1c2-35aca37d693c)|![Screenshot
    2025-06-06 at 14 02
    27](https://github.com/user-attachments/assets/55b36217-e5d3-46a9-b6ef-a377a3b5b365)|

--- src/generic/numberUtils.tsx
+++ src/generic/numberUtils.tsx
@@ -14,3 +14,22 @@ export function getCurrencyCompactLong(num: number): string {
   }).format(10);
   return currencyFormat.replace(/\p{Decimal_Number}+/u, numCompactLong);
 }
+
+export function numberToSigFigs(num: number, sigFigs: number): number {
+  if (num === 0) return 0;
+  const factor = Math.pow(10, sigFigs - Math.ceil(Math.log10(Math.abs(num))));
+  return Math.round(num * factor) / factor;
+}
+
+/**
+ * When showing numbers there may be both very small and large numbers in the same context.
+ * While generally we'd want them to be compared with the same number of decimals (eg. 13.1% and 6.8%),
+ * when there are very small numbers (eg. 0.0023%) that will easily be rounded to oblivion (0.00%), so
+ * this function will still show small numbers.
+ */
+export function numberToFixedUnlessSmall(value: number, precision: number = 2): string {
+  if (value > 1) {
+    return value.toFixed(precision);
+  }
+  return value.toPrecision(precision);
+}
